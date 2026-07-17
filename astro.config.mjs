6ff8f17ad1ee08646c86183d9d1b1e2abd113aa5
Mike  Alberts <mikealberts@gmail.com>
2026-07-17 05:53:10 -0400

    Build for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   outDir: './docs',
-  site: 'https://ruletheweb.us/',
+  site: 'https://www.ruletheweb.us/',
   vite: {
     plugins: [tailwindcss()],
   },
